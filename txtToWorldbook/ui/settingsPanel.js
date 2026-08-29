62dcdf43cba9a8afc61ca827eb66c2f1553a8666
StoryWeaver <story@weaver.com>
2026-08-30 00:01:56 +0800

    A1.0: 标记 src 迁移分支版本

--- txtToWorldbook/ui/settingsPanel.js
+++ txtToWorldbook/ui/settingsPanel.js
@@ -87,7 +87,7 @@ ${buildApiConfigCard('director', '🎬 导演AI配置')}
     </div>`;
 }
 
-const PLUGIN_VERSION = 'v4.1.4';
+const PLUGIN_VERSION = 'A1.0';
 
 function buildPluginUpdateHtml() {
     return '';
